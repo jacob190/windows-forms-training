83dc110568026fc51dce6230852115d33d663516
Jakub Cybulak <44304362+jacob190@users.noreply.github.com>
2020-01-07 21:24:29 +0100

    initial commit with some code

---
+++ Processor.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProcessorApp
+{
+    class Processor
+    {
+        private string Name
+        {
+            get;
+            set;
+        };
+        private int Cores;
+        private int TDP;
+
+        public Processor()
+        {
+            Name = "-";
+            Cores = 0;
+            TDP = 0;
+        }
+
+        string Name
+    
+
+
+    
+
+    }
+}
---
+++ Processors.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProcessorApp
+{
+    class Processors
+    {
+        private List<Processor> ProcessorsList = new List<Processor>();
+
+
+
+    }
+}
